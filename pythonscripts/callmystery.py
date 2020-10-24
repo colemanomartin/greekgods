b7bcd03f1df44f76488b1008773dba8b06f0b06f
COLEMAN MARTIN <colemanmartin@COLEMANs-MBP.attlocal.net>
2020-10-24 14:51:05 -0500

    Oct24 14:50

--- pythonscripts/callmystery.py
+++ pythonscripts/callmystery.py
@@ -3,18 +3,23 @@
 # import the random module
 
 print('#show_version=5')
-for i in range(7,37):
+r = 66
+for i in range(7,40):
    print('- duration: 1')
    print('  lights:')
-   print('    l_orbit_{0}: FFFFFFFF'.format(i+2))
-   print('    l_orbit_{0}: FFFFFFFF'.format(i+61))
-   print('    l_orbit_{0}: FFFFFFFF'.format(i+1))
-   print('    l_orbit_{0}: FFFFFFFF'.format(i+62))
-   print('    l_orbit_{0}: FFFFFFFF'.format(i+0))
-   print('    l_orbit_{0}: FFFFFFFF'.format(i+63))
-   print('    l_orbit_{0}: FFFFFF00'.format(i-1))
-   print('    l_orbit_{0}: FFFFFF00'.format(i+64))
-   print('    l_orbit_{0}: FFFFFF00'.format(i-2))
-   print('    l_orbit_{0}: FFFFFF00'.format(i+65))
+   if (i<37):
+     print('    l_orbit_{0}: FFFFFFFF'.format(i))
+     print('    l_orbit_{0}: FFFFFFFF'.format(r))
+   if (i<36):
+     print('    l_orbit_{0}: FFFFFFFF'.format(i-1))
+     print('    l_orbit_{0}: FFFFFFFF'.format(r+1))
+   if (i<35):
+     print('    l_orbit_{0}: FFFFFFFF'.format(i-2))
+     print('    l_orbit_{0}: FFFFFFFF'.format(r+2))
    print('    l_orbit_{0}: FFFFFF00'.format(i-3))
-   print('    l_orbit_{0}: FFFFFF00'.format(i+66))
+   print('    l_orbit_{0}: FFFFFF00'.format(r+3))
+   print('    l_orbit_{0}: FFFFFF00'.format(i-4))
+   print('    l_orbit_{0}: FFFFFF00'.format(r+4))
+   print('    l_orbit_{0}: FFFFFF00'.format(i-5))
+   print('    l_orbit_{0}: FFFFFF00'.format(r+5))
+   r = r -1
